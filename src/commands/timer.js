a730814b2a879b913ad28fbaf614adc7e4f96e4e
Peter Vertesi <vpeter1119@gmail.com>
2020-04-15 23:56:16 +0200

    Added timer parameters: countdown time in seconds and -m for message.

--- src/commands/timer.js
+++ src/commands/timer.js
@@ -1,19 +1,22 @@
+const Discord = require("discord.js");
+let sentMsg = new Discord.Message();
+var timer = null;
 
 module.exports = {
 	name: "timer",
 	aliases: ["countdown","cd"],
 	description: "Starts or stops a countdown timer.",
 	args: true,
-	usage: "timer [start|stop] [amount][s/m/h]",
+	usage: "timer [start|stop] [time in seconds]",
 	execute(msg, args) {
 		
 		const action = args.shift();
 		switch (action) {
 			case "start":
-				StartTimer(msg.channel, args);
+				StartTimer(msg, args);
 				break;
 			case "stop":
-				StopTimer(msg.channel, args);
+				StopTimer(msg);
 				break;
 			default:
 				return;
@@ -21,12 +24,38 @@ module.exports = {
 	}
 }
 
-function StartTimer(chn, args) {
+function StartTimer(message, args) {
 	//Start timer based on args
-	chn.send("This will start a timer.");
+	var timeToAdd = args[0];
+	var txt = "";
+	if (args.indexOf("-m") > -1) {
+		txt = args[((args.indexOf("-m"))+1)];
+	} else {
+		txt = "Timer";
+	}
+	if (timeToAdd > 3600) timeToAdd = 3600; 
+	var now = new Date().getTime();
+	var expireTime = now + (timeToAdd*1000);
+	var timeLeft = expireTime - now;
+	message.channel.send(`${txt}: ${timeLeft/1000} second(s) left`)
+	.then(msg => {
+		sentMsg = msg;
+	});
+	timer = setInterval(() => {	
+		timeLeft -= 1000;
+		sentMsg.edit(`${txt}: ${timeLeft/1000} second(s) left`);
+		if (timeLeft == 0) {
+			sentMsg.delete();
+			message.reply("your timer expired!");
+			clearInterval(timer);
+		} 
+	}, 1000);
 }
 
-function StopTimer(chn) {
+function StopTimer(message) {
 	//Stop timer
-	chn.send("This will stop the timer.");
+	if (timer === null) return;
+	sentMsg.delete();
+	clearInterval(timer);
+	message.channel.send("Timer stopped.");
 }
