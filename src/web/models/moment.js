8b362d0ea97067a469194447eb32ad61eb372d3a
Peter Vertesi <vpeter1119@gmail.com>
2020-04-18 21:27:29 +0200

    Add mongoose model "Moment"

---
+++ src/web/models/moment.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const options = {
+    //Insert any schema options here
+}
+
+const momentSchema = mongoose.Schema({
+  from: { type: String },
+  date: { type: Date },
+  text: { type: String }
+}, options);
+
+const Moment = mongoose.model("Moment", momentSchema);
+
+module.exports = Moment;
