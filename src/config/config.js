bc40deba25e127657c0c959e1de64ea3f34c957a
andrasadam93 <andrasadam93@gmail.com>
2020-04-19 01:19:30 +0200

    Changes to bot.js, and adding a global debug variable

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,7 @@ let config = require("./config.json");
 //// prefix is different for development and production environments
 config.token = process.env.TOKEN;
 config.prefix = process.env.PREFIX || config.prefix; //this should default to the character in config.json
+config.production = process.env.PRODUCTION == "FALSE" ? false : true;
 
 // This function returns the content of the config object, to be exported
 function ReturnConfig() {
