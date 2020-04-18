6c01e8df3b3a68f406238d6928677d6e8d15e27a
Peter Vertesi <vpeter1119@gmail.com>
2020-04-18 20:31:51 +0200

    Set up database connection.

--- src/web/index.js
+++ src/web/index.js
@@ -1,8 +1,33 @@
+
+// Require npm packages
 const express = require("express");
 const bodyParser = require("body-parser");
 require('dotenv').config();
 const app = express();
 const port = 3000;
+
+// Set up database connection
+const mongoose = require("mongoose");
+const mongoPw = process.env.MONGODB_PW;
+const mongoUser = process.env.MONGODB_USER;
+const mongoUrl = process.env.MONGODB_URL;
+mongoose.connect(
+    "mongodb+srv://" +
+    mongoUser +
+    ":" +
+    mongoPw +
+    mongoUrl,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    err => {
+        if (!err) {
+            console.log("Connected to database.");
+        } else {
+            console.log(err);
+            console.log("Did not connect to database.");
+        }
+    }
+);
+
 
 app.use(
     bodyParser.urlencoded({
